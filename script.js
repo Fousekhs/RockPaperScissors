19b68d375830b0f85510d0b1d7378c1c7961396a
Fousekis Dimitris <dimitris.fousekis.159@gmail.com>
2022-10-02 01:51:23 +0300

    added ui

--- script.js
+++ script.js
@@ -1,6 +1,98 @@
 
 const choices = ['Rock', 'Paper', 'Scissors']
 
+const btns = document.querySelectorAll("img");
+
+btns.forEach(
+    function(btn) {
+        btn.addEventListener("mouseover", () => {
+            btn.classList.add("onmouse");
+            const choice = document.querySelector("#choice");
+            choice.textContent = btn.id;
+        })
+
+        btn.addEventListener("mouseout", () => {
+            btn.classList.remove("onmouse");
+        })
+
+        btn.addEventListener("click", () => {
+            const table = document.querySelector(".pick_table");
+            table.hidden = true;
+            const container = document.querySelector(".container");
+
+            const result = showResult(btn.id);
+            container.appendChild(result);
+        })
+    }
+)
+
+function showResult(playerChoice) {
+    const result = document.createElement("div");
+
+    const presented = document.createElement("div");
+    presented.classList.add("buttons");
+
+    presented.appendChild(getElementByChoice(playerChoice));
+
+    const vs = document.createElement("div");
+    vs.style = "font-weight: 700; font-size: large";
+    vs.textContent = "VS";
+    presented.appendChild(vs);
+
+    const cpuChoice = getComputerChoice(); 
+    presented.appendChild(getElementByChoice(cpuChoice));
+    
+    result.appendChild(presented);
+
+    const table = document.createElement("table");
+    table.style = "width: 100%; text-align: center;"
+    const tr = document.createElement("tr");
+
+    const td1 = document.createElement("td");
+
+    const match = document.createElement("div");
+    match.style = "font-weight: 700; font-size: large";
+
+    const gameResult = getResult(cpuChoice, playerChoice);
+    if (gameResult == 0) {
+        match.textContent = "Draw";
+    } else if (gameResult == 1) {
+        match.textContent = "Player wins";
+    } else {
+        match.textContent = "Computer wins";
+    }
+    
+    td1.appendChild(match);
+    tr.appendChild(td1);
+
+    const td2 = document.createElement("td");
+
+    const button = document.createElement("button");
+    button.textContent = "Play Again";
+    result.appendChild(button);
+    
+    button.addEventListener("click", () => {
+        location.reload();
+    })
+
+    td2.appendChild(button);
+    tr.appendChild(td2);
+    table.appendChild(tr);
+
+    result.appendChild(table);
+
+
+    return result;
+
+}
+
+function getElementByChoice(choice) {
+    const img = document.createElement("img");
+    img.src = "images/" + choice.toLowerCase() + ".png";
+    img.alt = choice;
+    return img; 
+}
+
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
@@ -9,7 +101,7 @@ function capitalise(str) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-function result(computerSelection, playerSelection) {
+function getResult(computerSelection, playerSelection) {
     playerSelection = capitalise(playerSelection);
 
     if (playerSelection === computerSelection) {
@@ -37,24 +129,3 @@ function result(computerSelection, playerSelection) {
         return 2;
     }
 }
-
-function playRound() {
-    const computerSelection = getComputerChoice();
-    const playerSelection = prompt('Enter your choice: Rock-Paper-Scissors');
-    return result(computerSelection, playerSelection);
-}
-
-function game() {
-    let winner = [0, 0, 0];
-    for (let i = 0; i < 5; i++) {
-        winner[playRound()]++;
-    }
-    if (winner[1] > winner[2]) {
-        alert('player won');
-    } else {
-        alert('computer won');
-    }
-    alert(winner[1] + '/' + winner[0] + '/' + winner[2] + ' player wins/draws/computer wins');
-}
-
-game();
